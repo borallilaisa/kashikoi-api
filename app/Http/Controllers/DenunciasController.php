ba9b14c77610c5f29580cdb94ce04983a9f8e938
laisa boralli <borallilaisa@gmail.com>
2021-01-25 18:51:20 -0300

    Graficos

--- app/Http/Controllers/DenunciasController.php
+++ app/Http/Controllers/DenunciasController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Denuncia;
+use App\Models\Notification;
 use App\Models\User;
 use App\Notifications\AvisarDenunciado;
 use Illuminate\Http\Request;
@@ -55,21 +56,45 @@ class DenunciasController extends Controller
     public function confirmDenuncia(Denuncia $denuncia, Request $request) {
 
         $denuncia->denunciado->delete();
+
+
         $denuncia->denunciado->notify(new AvisarDenunciado());
+
 
         $denuncia->delete();
 
 
-        return json_encode($denuncia);
+        $outrasDenuncias = Denuncia::when($denuncia, function($query) use ($denuncia){
+            return $query->where('usuarioDenunciado', $denuncia->denunciado->id);
+        })->get();
 
+        foreach($outrasDenuncias as $OutraDenuncia) {
+            $OutraDenuncia->delete();
+        }
+
+
+
+        return json_encode($denuncia);
+        return json_encode($outrasDenuncias);
     }
 
     public function ignoreDenuncia(Denuncia $denuncia, Request $request) {
 
       $denuncia->delete();
 
+
+        $notification = new Notification();
+
+        $notification->store([
+            'titulo' => 'Resposta de Análise de Denuncia',
+            'mensagem' => 'Olá! Sua denuncia foi analisada e por não encontrarmos nenhuma irregularidade, então descartamos seu registro. Caso possua alguma dúvida, por gentileza, entrar em contato com a equipe Kashikoi.',
+            'id_destinatario' => $denuncia->usuarioDenunciador
+        ]);
+
         return json_encode($denuncia);
 
     }
+
+
 
 }

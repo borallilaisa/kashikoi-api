33a3bb93fde916f85b3d8b7b0a5f2d04c7d6d828
laisa boralli <borallilaisa@gmail.com>
2021-03-20 23:32:39 -0300

    CORS

--- routes/api.php
+++ routes/api.php
@@ -3,8 +3,8 @@ use App\Http\middleware\CheckApiToken;
 use Illuminate\Http\Request;
 
 header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
-header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+header('Access-Control-Allow-Methods: *');
+header('Access-Control-Allow-Headers: *');
 
 Route::post('/user/recuperar-senha',                'UserController@recuperarSenha');
 Route::post('/user/recuperar-senha-finalizar',      'UserController@recuperarSenhaFinalizar');

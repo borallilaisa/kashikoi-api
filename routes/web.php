ff2ef059fcc81682f0fe89efc64c38a6e395546d
laisa boralli <borallilaisa@gmail.com>
2021-06-29 20:50:30 -0300

    ULTIMO AJUSTE

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,10 @@ Route::get('/auth/facebook/callback',           'UserController@facebookLoginCal
 Route::get('/auth/google/login',              'UserController@googleLogin');
 Route::get('/auth/google/callback',           'UserController@googleLoginCallback');
 
-Route::get('/print-pdf', 'PDFCOntroller@printPdf');
-Route::get('/print/numero-conversas-dia', 'PDFCOntroller@numeroConversasDia');
-Route::get('/print/numero-amizades-dia', 'PDFCOntroller@numeroAmizadesDia');
-Route::get('/print/numero-usuarios-dia', 'PDFCOntroller@numeroUsuarioDia');
-Route::get('/print/numero-denuncias-dia', 'PDFCOntroller@numeroDenunciaDia');
-Route::get('/print/assuntos-mais-populares', 'PDFCOntroller@assuntosMaisPopulares');
-Route::get('/print/total-conversas-trimestre', 'PDFCOntroller@totalConversasTrimestre');
+Route::get('/print-pdf', 'PDFController@printPdf');
+Route::get('/print/numero-conversas-dia', 'PDFController@numeroConversasDia');
+Route::get('/print/numero-amizades-dia', 'PDFController@numeroAmizadesDia');
+Route::get('/print/numero-usuarios-dia', 'PDFController@numeroUsuarioDia');
+Route::get('/print/numero-denuncias-dia', 'PDFController@numeroDenunciaDia');
+Route::get('/print/assuntos-mais-populares', 'PDFController@assuntosMaisPopulares');
+Route::get('/print/total-conversas-trimestre', 'PDFController@totalConversasTrimestre');
